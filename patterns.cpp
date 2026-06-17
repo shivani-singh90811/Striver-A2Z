e9653195d887fe7064aa3c62c57b855294c802aa
Shivani Singh <shivanikumari200631@gmail.com>
2026-06-17 13:08:13 +0530

    Day 1 patterns

---
+++ patterns.cpp
@@ -0,0 +1,120 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void print1(int n) {
+
+    for (int i = 0; i < n; i++) {
+        cout << "*";
+    }
+    cout << endl;
+}
+void print2(int n) {
+    for (int i = 0; i < n; i++) {
+    for (int j = 0; j <=i; j++) {
+        cout << "*";
+    }
+    cout << endl;
+}
+}
+void print3(int n) {
+    for (int i = 1; i <= n; i++) {
+    for (int j = 1; j<=i; j++) {
+        cout << j<<" ";
+    }
+    cout << endl;
+}
+}
+void print4(int n) {
+    for (int i = 1; i <= n; i++) {
+    for (int j = 1; j<=i; j++) {
+        cout << i <<" ";
+    }
+    cout << endl;
+}
+}
+void print5(int n) {
+
+    for (int i = 1; i <= n; i++) {
+    for (int j = 1; j<=n-i+1; j++) {
+        cout << "*" <<" ";
+    }
+
+    cout << endl;
+}
+}
+void print6(int n) {
+    
+    for (int i = 1; i <= n; i++) {
+    for (int j = 1; j<=n-i+1; j++) {
+        cout << j <<" ";
+    }
+
+    cout << endl;
+}
+}
+void print7(int n) {
+    
+    for (int i = 0; i <n; i++) {
+
+        // space 
+    for (int j = 0; j<=n-i-1; j++) {
+        cout <<" ";
+    }
+    // star
+    for(int j = 0; j<2*i+1; j++) {
+        cout << "*";
+    }
+    for(int j = 0; j<n-i-1; j++) {
+        cout <<" ";
+    }
+
+    cout << endl;
+}
+}
+void print8(int n) {
+    
+    for (int i = 0; i <n; i++) {
+
+        // space 
+    for (int j = 0; j<i; j++) {
+        cout <<" ";
+    }
+    // star
+    for(int j = 0; j<2*n-(2*i+1); j++) {
+        cout << "*";
+    }
+    for(int j = 0; j<i; j++) {
+        cout <<" ";
+    }
+
+    cout << endl;
+}
+}
+void print9(int n) {
+    
+    for (int i = 0; i <=2*n-1; i++) {
+    }
+    int stars = i ;
+     if(i > n)
+    stars = 2*n-i;
+    
+    for (int j =1; j<=stars; j++) {
+        cout << "*";
+    }
+
+    cout << endl;
+}
+
+
+int main(){
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    int t;
+    cin >> t;
+    for(int i = 0;i<t;i++){
+        int n;
+        cin >> n;
+        print7(n);
+        print8(n);
+    }
+}
