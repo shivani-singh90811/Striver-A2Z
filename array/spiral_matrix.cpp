a7f46039bb80b177f0f032c0009b0ff093573225
Shivani Singh <shivanikumari200631@gmail.com>
2026-07-19 23:40:30 +0530

    DAY 33 of learning DSA

---
+++ array/spiral_matrix.cpp
@@ -0,0 +1,57 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+vector<int> spiralMatrix(vector<vector<int>> &mat){
+    int n = mat.size();
+    int m = mat[0].size();
+    int left = 0, right = m-1;
+    int top = 0, bottom = n-1;
+    vector<int> ans;
+
+    while(top <= bottom && left <= right){
+        // top row
+        for(int i = left; i <= right; i++){
+            ans.push_back(mat[top][i]);
+        }
+        top++;
+
+        // right column
+        for(int i = top; i <= bottom; i++){
+            ans.push_back(mat[i][right]);
+        }
+        right--;
+
+        // bottom row
+        if(top <= bottom){
+            for(int i = right; i >= left; i--){
+                ans.push_back(mat[bottom][i]);
+            }
+            bottom--;
+        }
+
+        // left column
+        if(left <= right){
+            for(int i = bottom; i >= top; i--){
+                ans.push_back(mat[i][left]);
+            }
+            left++;
+        }
+    }
+    return ans;
+}
+
+int main(){
+    vector<vector<int>> mat = {
+        {1,  2,  3,  4},
+        {5,  6,  7,  8},
+        {9,  10, 11, 12},
+        {13, 14, 15, 16}
+    };
+
+    vector<int> result = spiralMatrix(mat);
+    for(int x : result){
+        cout << x << " ";
+    }
+    cout << endl;
+    return 0;
+}
